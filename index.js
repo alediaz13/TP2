357861ed06491ded2bf4a0e4ba144f453e8498b8
Alejandro Barrera <alekey012@gmail.com>
2024-05-30 14:53:11 -0300

    Iniciando estructura en JS

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById('CalcularBoton').addEventListener('click', function() {
+    const estatura = parseFloat(document.getElementById('estatura').value);
+    const peso = parseFloat(document.getElementById('peso').value);
+})
