f10b526f873cc7639057c454484bda090b4b072f
Alejandro Barrera <alekey012@gmail.com>
2024-05-30 15:08:23 -0300

    Ejercicio4 terminado

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 document.getElementById('CalcularBoton').addEventListener('click', function() {
     const estatura = parseFloat(document.getElementById('estatura').value);
     const peso = parseFloat(document.getElementById('peso').value);
-})
+
+    if(isNaN(estatura) || isNaN(peso)) {
+        alert('Ingresa valores validos para estrcutura y el peso')
+        return;
+    }
+
+    const imc = peso / (estatura * estatura);
+
+    alert('Tu IMC es: ' + imc.toFixed(2));
+});
